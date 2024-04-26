a250e1c34b3d465aa710a01e9cc683a21ecf3eeb
fatman148 <108585064+tehpez148@users.noreply.github.com>
2024-04-26 17:39:35 +0100

    Tidied up layout for JS file to make it easier to read and to understand. Hopefully.

--- ttt.js
+++ ttt.js
@@ -1,5 +1,5 @@
 
-
+//gameboard factory 
 const gameBoard = ( function() {
 
     const rows = 3;
@@ -17,7 +17,7 @@ const gameBoard = ( function() {
  
 
     const getBoard = board;
-
+      //fills board array with values from cell function, either a '0' or their player token
     const printBoard = () => {
       const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
       return boardWithCellValues
@@ -25,7 +25,7 @@ const gameBoard = ( function() {
     };
 
   
-
+      //drops a player token based on their selection
     const dropToken = (pick, player) => { 
       let pickI = parseInt(pick);
       console.log(`dropToken function has found ${pickI}`);
@@ -81,17 +81,16 @@ function Cell(){
 
 
 
-
+//logs board just to check 
 console.log(gameBoard.printBoard())
 
 
 
-
+//controller to play the game
 const playGame = ( function(){
 
-    let boardForChecks = [1,2,3,4,5,6,7,8,9];
 
-
+    //array to hold player information, also color for their tokens <@:)
     const players = [
         {
           name: "Player One",
@@ -105,17 +104,15 @@ const playGame = ( function(){
         }
       
       ];
-        
+        //calls drop token function, also logs it to keep an eye on things. 
     const playerMove = (player,pick) => {
-        //let bArray = boardForChecks;
-            //while (bArray.includes(parseInt(pick)) === false) {
            
             console.log(`${player.name} did it! Their ${player.token} was dropped!`);
-            //delete bArray[(pick - 1)];
             gameBoard.dropToken(pick, player.token);
             checkWin(getActivePlayer());
 
     };
+    //check win function, called after each pick. 
     const checkWin = (player) => {
       // horizontal winners 
       console.log("checkWin is happening now :)")
@@ -161,8 +158,8 @@ const playGame = ( function(){
         && gameBoard.printBoard() [2][0])
         {winnerDiv.textContent = (`${player.name} has succeeded!`);
         dialog.showModal();}
-      else if (boardForChecks.length === 0)
-      {winnerDiv.textContent = ("It's a draw!")}
+      //else if (boardForChecks.length === 0)
+      //{winnerDiv.textContent = ("It's a draw!")}
       //if no win yet, calls changePlayerTurn function and allows next player to pick a button. 
       else {
         changePlayerTurn();
@@ -173,26 +170,14 @@ const playGame = ( function(){
 
       }
     };
-
+    //changes player based on who is current player, very pleased with this. 
     let activePlayer = players[0];
     
     const changePlayerTurn = () => {
       activePlayer = activePlayer  === players[0] ? players[1] : players [0];
-  
-    }
+    };
 
     const getActivePlayer = () => activePlayer;
-    
-    /*const playRound = () => {
-
-        playerMove(getActivePlayer(),);
-
-        checkWin(getActivePlayer());
-
-        return;
-      };                      
-
-*/
 
 
 
@@ -205,11 +190,12 @@ const playGame = ( function(){
 playGame.playRound;
 
 
-
+//selects all buttons in play grid 
 const buttons = document.querySelectorAll(".griddies");
 
 
-
+//cheeky function that does alot, takes player selection and put it into game logic, adds player token/color to board, 
+//disables that "position" on the board.
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     let obj = document.activeElement;
@@ -223,12 +209,13 @@ buttons.forEach(function(button) {
   })
 });
 
-
+//takes current active player and appends it to appropriate div, called to begin with to populate DOM
 const displayPlayer = document.getElementById('turn');
 let startingPlayer = document.createElement('p');
 startingPlayer.textContent = playGame.getActivePlayer().name;
 displayPlayer.appendChild(startingPlayer);
 
+//changes active player div as needed 
 function showPlayer (player){
   let oldPlayer = displayPlayer.querySelector('p');
   oldPlayer.remove();
@@ -238,22 +225,24 @@ function showPlayer (player){
   displayPlayer.appendChild(currentPlayer);
 };
 
-
+//reloads the whole page. 
 const reloadBut = document.getElementById('reset');
 reloadBut.addEventListener('click', () => {
   location.reload();
 });
 
+//adds the result of checkWinner function to a dialog box
 const dialog = document.querySelector('dialog');
 const winnerDiv = document.createElement('p');
 dialog.appendChild(winnerDiv);
-
+//adds a reset button to the winner dialog box
 const dialogReleodButton = document.querySelector("dialog button");
 dialogReleodButton.addEventListener('click', () => {
   location.reload();
 });
 
-
+//two dialog boxes to changes the names of both players, player1 or player 2. 
+//calls show player function to update if needed. 
 const dialog1 = document.getElementById("name1form");
 const showdialog1 = document.getElementById("player1name");
 
@@ -265,6 +254,7 @@ const new1name = document.getElementById("name1form-submit");
 new1name.addEventListener("click", () =>{
   let newName = document.getElementById("one-name").value;
   playGame.players[0].name = newName;
+  showPlayer(playGame.getActivePlayer());
 });
 
 const dialog2 = document.getElementById("name2form");
@@ -278,4 +268,5 @@ const new2name = document.getElementById("name2form-submit");
 new2name.addEventListener("click", () =>{
   let newName = document.getElementById("two-name").value;
   playGame.players[1].name = newName;
+  showPlayer(playGame.getActivePlayer());
 });
